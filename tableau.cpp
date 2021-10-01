d0b21fe0489fb63355b01013f5c4d7d4328c65b6
jordanRoland1 <90456915+jordanRoland1@users.noreply.github.com>
2021-10-01 12:07:29 +0200

    Mise a jour

--- tableau.cpp
+++ tableau.cpp
@@ -13,7 +13,7 @@ int main()
 	use.afficher();
 	std::cout << std::endl << "Voici la valeurs minimale : " << use.minimum() << std::endl;
 	std::cout << "Voici la valeurs maximale : " << use.maximum() << std::endl;
-	std::cout << "Veillez saisir 10 valeurs" << std::endl;				// demande et écrit dans le tableau
+	std::cout << "Veillez saisir 10 valeurs" << std::endl;				// demande et Ã©crit dans le tableau
 	use.modifier();
 	use.afficher();
 	std::cout << "Quelle case voulez-vous afficher ?" << std::endl;
@@ -28,29 +28,6 @@ int main()
 	use.afficher();
 	std::cout << std::endl << "Voici la valeurs minimale : " << use.minimum() << std::endl;
 	std::cout << "Voici la valeurs maximale : " << use.maximum() << std::endl;
-	//
-
-//	std::cout << std::endl << std::endl << std::endl << std::endl;
-//	int nombre = 0;
-//	srand(time(NULL));
-//	int T[3][4] = {};
-//			for (int i(0); i < 3; i++)
-//			{
-//				for (int j(0); j < 4; j++)
-//				{
-//					nombre = rand() % 2;
-//					T[i][j] = nombre;
-//				}
-//			}
-
-//	for (int i(0); i < 3 ; i++)
-//	{
-//		for (int j (0); j < 4; j++)
-//		{
-//			std::cout << " || " << T[i][j];
-//		}
-//		std::cout << std::endl;
-//	}
 	return 0;
 
 
